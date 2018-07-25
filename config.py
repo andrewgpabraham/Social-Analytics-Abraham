d8c29b94b0079f9d0676f830c176c7ce3aa76aea
Andrew Abraham <andrewgpabraham@gmail.com>
2018-07-25 13:57:15 -0400

    remove config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "KNo7OjsVHD1cn79WD7iLZaqdK"
-consumer_secret = "WHmZA8I36JdttbmKVJYuklQcYhsissjbCFs2i7pleodKkBJK6F"
-access_token = "25390742-P2kFr5MeMNaBrNUuzgpQcYaEnauNNGn02iVx6JXG8"
-access_token_secret = "OCwHNSnMjpUx6HjY8hZHGS9QV2IqoDvhdc7VNWhJAWKJg"
